0d28f62b380a2c3983d749964b36a0b3311e90e7
Ynda Jas <yndajas@gmail.com>
2020-07-17 02:17:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
   attr_accessor :name :artist :genre
   
   @@song_count = 0
+  @@artists = []
   
   def initialize(song)
     @@song_count++
@@ -10,6 +11,10 @@ class Song
   
   def self.count
     @@song_count
+  end
+  
+  def self.artists
+    @@artists
   end
   
 end
